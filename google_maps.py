21fd80eef3fb22c3207728095ef14cdd8dc75fb7
maricr-sk <85328841+maricr-sk@users.noreply.github.com>
2024-06-26 19:33:36 -0400

    stylistic fixes

--- google_maps.py
+++ google_maps.py
@@ -1,5 +1,4 @@
 import googlemaps
-import requests
 from dotenv import load_dotenv
 import os
 
@@ -9,23 +8,34 @@ load_dotenv()
 GMAPS_K = os.environ.get('GMAPS_KEY')
 gmaps = googlemaps.Client(key=GMAPS_K)
 
-def get_recs(zip): # def get_recs(query, zip):
+
+def get_recs(zip):  # def get_recs(query, zip):
     try:
         geocode_result = gmaps.geocode(zip)[0]['geometry']['location']
-        querie =  {
+        querie = {
             "textQuery": f"Spicy Vegetarian Food near {geocode_result}"
         }
         '''
         url = 'https://places.googleapis.com/v1/places:\f{query}'
         '''
-        response = gmaps.places(query=querie, location=geocode_result, type="restaurant", open_now=True)
-        
+        response = gmaps.places(
+            query=querie,
+            location=geocode_result,
+            type="restaurant",
+            open_now=True
+            )
+
         '''
         recs = {}
 
         for r in response['results']:  # if needed
             # potential fields of interest:
-            recs[r['name']] = (r['formatted_address'], r['rating'], r.get('price_level', 'Unknown'))
+            recs[r['name']] =
+            (r['formatted_address'],
+                r['rating'],
+                r.get('price_level',
+                'Unknown')
+            )
 
         return recs
         '''
@@ -34,5 +44,6 @@ def get_recs(zip): # def get_recs(query, zip):
 
     except googlemaps.exceptions.ApiError as e:
         print(f"Google Maps API Error occurred: {e}")
-    
+
+
 print(get_recs('10027'))
