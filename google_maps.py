8e8fd59cca00e5d4cdb6e66199f6938edaaefcef
maricr-sk <85328841+maricr-sk@users.noreply.github.com>
2024-06-26 18:43:18 -0400

    had to rename to google_maps (circular dependency), finished querying code

---
+++ google_maps.py
@@ -0,0 +1,33 @@
+import googlemaps
+import requests
+from dotenv import load_dotenv
+import os
+
+# need to call Google Maps API and see the values it returns
+
+load_dotenv()
+GMAPS_K = os.environ.get('GMAPS_KEY')
+gmaps = googlemaps.Client(key=GMAPS_K)
+
+def get_recs(zip): # def get_recs(query, zip):
+    try:
+        geocode_result = gmaps.geocode(zip)[0]['geometry']['location']
+        querie =  {
+            "textQuery": f"Spicy Vegetarian Food near {geocode_result}"
+        }
+        '''
+        url = 'https://places.googleapis.com/v1/places:\f{query}'
+        '''
+        response = gmaps.places(query=querie, location=geocode_result, type="restaurant", open_now=True)
+        
+        recs = {}
+
+        #for r in response['results']: 
+        # potential fields of interest: ['name'], ['formatted address'], ['price_level'], ['rating']
+
+        return response
+
+    except googlemaps.exceptions.ApiError as e:
+        print(f"Google Maps API Error occurred: {e}")
+    
+print(get_recs('10027'))
