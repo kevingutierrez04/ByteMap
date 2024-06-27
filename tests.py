88ba43ac49512046b1b11bb2252f6042e09c2684
maricr-sk <85328841+maricr-sk@users.noreply.github.com>
2024-06-27 18:12:14 -0400

    forgot to add unittest function

---
+++ tests.py
@@ -0,0 +1,19 @@
+# assertEqual
+# assertNotEqual
+# assertTrue
+
+import unittest
+from main import getInput, callChat, printResults
+from google_maps import get_recs
+
+class TestFileName(unittest.TestCase):
+  def test_function1(self):
+    # self.assertEqual(function1(1), 0)
+    results = getInput()
+    
+
+  def test_function2(self):
+    # self.assertEqual(function2(2,1), 3)
+    # self.assertEqual(function2(2.1, 1.2), 3.3)
+
+
