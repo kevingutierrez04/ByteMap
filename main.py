a9372c4b6fb380165450ba6159c8c05363237847
acabrera04 <acabrera04164@gmail.com>
2024-06-27 18:01:07 -0400

    Removed chatgpt

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-from openai import OpenAI
 from dotenv import load_dotenv
 import re
 from google_maps import get_recs
@@ -33,47 +31,6 @@ def getInput():
             )
     sorting = input("Would you like the results sorted by Prominence (Default), distance, or ratings?: ")
     return x, y, sorting
-
-def callChat():
-    # initialize client with API key from .env
-    client = OpenAI(
-        api_key=os.getenv('OPENAI_API_KEY'),
-    )
-    # Starting a chat with GPT3.5
-    stream = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": '''
-    Based off the user input, return an appropriate query\
-    (of 4 words or less)\
-    that could be inputted into Google Maps.
-    The user is looking for places to eat. \
-    Please take into account what type of food they want.
-    For example, if the person inputs "meatless" \
-    a good query would be something like \
-    "vegan food near me"
-    '''
-            },
-            {"role": "system", "content":'''
-            Now, please describe to me some dietary preferences
-            '''},
-            {"role": "user", "content": '''
-            I would like some {y} food
-            '''},
-            {"role": "system", "content": '''
-            Great, Heres some {y} food near you
-            '''}],
-        stream=True,
-    )
-
-    # regex for 5 digit zip code, /^\d{5}$/
-    # https://skillforge.com/how-to-use-javascript-regular-expressions/#:~:text=So%20%5E%5Cd%7B5%7D,must%20start%20with%20five%20numbers.
-    test = ""
-    for chunk in stream:
-        if chunk.choices[0].delta.content:
-            test += chunk.choices[0].delta.content
-    # ans = json.loads(test)
-    print(test)
 
 def printResults(zipcode, food, sort):
     prices = {
