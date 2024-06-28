35a36c2a5636d3d424fd5a0b889facf1be4c35c3
maricr-sk <85328841+maricr-sk@users.noreply.github.com>
2024-06-28 10:33:50 -0400

    made some stylistic changes, added to unittests

--- main.py
+++ main.py
@@ -7,32 +7,38 @@ import sqlalchemy as db
 # import json
 
 load_dotenv()
+
+
+class create():
+    def __init__(self, ans='y'):
+        self.an = ans
+
+
 def getInput():
     zipcode = False
-
+    print()
     while not zipcode:
-        x = input('''
-    Hello! Welcome to ByteMap. I'm your personalized AI Assistant ready
-    to provide local restaurant reccomendations.
-    Please describe your preferences.
-    Firstly, what is your zip code?
-    '''
-                )
+        print()
+        x = input(
+            "Hello! Welcome to ByteMap. I'm your personalized "
+            '''assistant ready to provide local restaurant recommendations.'''
+            "\nPlease describe your preferences. Firstly, what is your"
+            " zip code?\n")
 
         pattern = r"^\d{5}$"
         match = re.match(pattern, x)
         if match:
-            print(match)
             zipcode = True
 
-    y = input('''
-    Now, please describe to me some dietary preferences.
-    '''
-            )
-    sorting = input("Would you like the results sorted by Prominence (Default), distance, or ratings?: ")
+    y = input("Now, please describe to me some dietary preferences.\n")
+
+    sorting = input("Would you like the results sorted by"
+                    " Prominence (Default), distance, or ratings?: \n")
     return x, y, sorting
 
+
 def printResults(zipcode, food, sort):
+
     prices = {
         "0.0": "Free",
         "1.0": "<= $10",
@@ -54,36 +60,45 @@ def printResults(zipcode, food, sort):
     df = df.astype(str)
     df['price_level'] = df['price_level'].map(prices)
 
-    df.to_sql('Reccomendations', con=engine, if_exists='replace', index=False)
+    df.to_sql('Recommendations', con=engine, if_exists='replace', index=False)
     vicinity_renamed = False
 
-    print("Here are the Top 5 Reccomended Restaurants near you")
+    print("Here are the Top 5 Recommended Restaurants near you")
 
     with engine.connect() as connection:
         if not vicinity_renamed:
-            connection.execute(db.text("ALTER TABLE Reccomendations RENAME COLUMN vicinity to address;"))
+            connection.execute(db.text("ALTER TABLE Recommendations RENAME \
+                                       COLUMN vicinity to address;"))
             vicinity_renamed = True
         if sort == "ratings":
             table = connection.execute(
-                db.text("SELECT name, address, rating, price_level FROM Reccomendations ORDER BY rating DESC;")
+                db.text("SELECT name, address, rating, price_level FROM \
+                        Recommendations ORDER BY rating DESC;")
                 )
         else:
             table = connection.execute(
-                db.text("SELECT name, address, rating, price_level FROM Reccomendations;")
+                db.text("SELECT name, address, rating, price_level FROM \
+                        Recommendations;")
                 )
+
+        cr = create()
         ans = "y"
         while ans != "n":
-            
             query_result = table.fetchmany(5)
             if len(query_result) == 0:
-                print("There are no more reccomendations")
+                print("There are no more Recommendations")
                 break
             print(pd.DataFrame(query_result))
             print()
-            ans = input("Would you like more reccomendations? (y/n): ")
+            if cr.an == 'n':
+                ans = 'n'
+            else:
+                ans = input("Would you like more Recommendations? (y/n): ")
             print()
 
-    print("Thank you for using ByteMap, I hope that you have a wonderful meal from the restaurant you chose")
+    print("Thank you for using ByteMap, I hope that you enjoy the meal"
+          " from the restaurant you chose!\n\n")
+
 
 if __name__ == "__main__":
     zipcode, food, sort = getInput()
--- tests.py
+++ tests.py
@@ -1,19 +1,38 @@
-# assertEqual
-# assertNotEqual
-# assertTrue
 
 import unittest
-from main import getInput, callChat, printResults
-from google_maps import get_recs
+from main import printResults, create
+
 
 class TestFileName(unittest.TestCase):
-  def test_function1(self):
-    # self.assertEqual(function1(1), 0)
-    results = getInput()
-    
+    cr = create()
+    cr.an = "n"
 
-  def test_function2(self):
-    # self.assertEqual(function2(2,1), 3)
-    # self.assertEqual(function2(2.1, 1.2), 3.3)
+    def get_Input(diet, sorti="prominence", zipcode=10027):
+        return zipcode, diet, sorti
 
+    def test_function1(self):
+        # self.assertEqual(function1(1), 0)
+        results1 = printResults('vegan', "prominence", 10027)
+        results2 = printResults('vegan')
+        results3 = printResults('vegan', "sorting", 10027)
+        results4 = printResults('vegan', "ratings", 10027)
+        results5 = printResults('vegan', zipcode=10025)
 
+        '''self.assertEqual(results1, )
+        self.assertEqual(results2, 3.3)
+        self.assertEqual(results3, 3.3)
+        self.assertEqual(results4, 3.3)
+        self.assertEqual(results5, 3.3)'''
+
+    def test_function2(self):
+        results1 = printResults('hamburgers')
+        results2 = printResults('american')
+        results3 = printResults('dessert')
+        results4 = printResults('mongolian')
+        results5 = printResults('nutfree')
+
+        '''self.assertEqual(results1, 3)
+        self.assertEqual(results2, 3.3)
+        self.assertEqual(results3, 3.3)
+        self.assertEqual(results4, 3.3)
+        self.assertEqual(results5, 3.3)'''
